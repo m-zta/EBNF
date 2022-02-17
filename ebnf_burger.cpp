2d1d4f7122f04321df174e743b570cb420bfa2c3
Mario Knaus <mario@Marios-MacBook-Air.local>
2022-02-17 02:15:04 +0100

    added auto test mode

--- ebnf_burger.cpp
+++ ebnf_burger.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 #include "ebnf_helpers.cpp"
 
 // Krusty Krab Burger:
-// Burger = Bun Salad (Onions)
+// Burger = Bun Salad {Onions}
 //          '(' Patty Cheese {Patty | Cheese} ')'
 //          Sauce Bun
 // Salad = 'S'
-// Onions = Onion {Onions}
-// Onion = 'O'
+// Onions = 'O' 'O' 'O'
 // Patty = 'X' | 'Y'
 // Cheese = 'C'
 // Sauce = 'K' | 'M' | 'T'
@@ -35,8 +36,7 @@ bool is_pc(std::istream& is) {
 }
 
 bool is_onions(std::istream& is) {
-    if(lookahead(is) != 'O') return true;
-    return consume(is, 'O') && is_onions(is);
+    return consume(is, 'O') && consume(is, 'O') && consume(is, 'O');
 }
 
 bool is_burger(std::istream& is) {
@@ -52,10 +52,53 @@ bool is_burger(std::istream& is) {
 }
 
 int main() {
+    /* 
+    //this is manual test mode
     if(is_burger(std::cin)) {
         std::cout << "valid\n";
     } else {
         std::cout << "invalid\n";
     }
     return 0;
+    */
+
+    // this is auto-test mode
+    
+    std::vector<std::string> testcon {
+        "BSOOO(XC)KB", // valid
+        "BSOOO(XCYCCXC)KB", // valid
+        "BSOOOOOO(XCYXY)MB", // valid
+        "BSOOO(YC)MB", // valid
+        "BSOO(XCYXC)TB", // valid
+        "BSOOOOOOOOO(YCXXCCX)KB", // valid
+        "BSOO(XCKB ", // invalid
+        "BSOO(XC)MB", // invalid
+        "BSOOO(YXC)KB", // invalid
+        //"BSOOO(CYCC)MB", // invalid, this one causes a segmentation fault...
+        "BOOOOOOOO(XC)MB", // invalid
+        "BOOO(XCXXXCC)TKB", // invalid
+        "BOOOYCYYY)KB", // invalid
+        "BBOOO(XC)TB", // invalid
+        "BOOO(XCCCCY)KBBB" // invalid
+    };
+
+    std::cout << "\nAUTOTEST RUN:\n";
+    unsigned int failed {0};
+
+    for (unsigned int i {0}; i < testcon.size(); ++i) {
+        std::string str {testcon.at(i)};
+        std::istringstream is {str};
+        bool val {i < 6};
+        bool res {is_burger(is)};
+
+        std::cout << '\n' << i + 1 << ". input: " << str << std::endl;
+        std::cout << "expected output: " << (val ? "valid" : "invalid") << std::endl;
+        std::cout << "actual output: " << (res ? "valid" : "invalid") << std::endl;
+        std::cout << ((i < 6 == res) ? "PASSED" : "--NOT PASSED") << std::endl;
+    }
+
+    // FIXME: Solve the problem of type conversion from unsigned long to double...
+    // double rate {(testcon.size() - failed) / testcon.size() * 100};
+    //     std::cout << "\ntotal result: " << rate << "%\n";
 }
+
