69f6c05219583dc13e434ede2d67a8987d207116
Mario Knaus <mario@student-net-etx-0689.intern.ethz.ch>
2022-02-21 08:17:33 +0100

    minor changes

--- train.cpp
+++ train.cpp
@@ -21,25 +21,25 @@ bool composition(std::istream& is);
 
 bool composition(std::istream& is) {
     // immediately exit if first character is not '<'
-    if(!consume(is, '<')) return false;
+    if (!consume(is, '<')) return false;
 
-    if(!open(is) || !loco(is)) return false;
+    if (!open(is) || !loco(is)) return false;
 
-    if(!consume(is, '>')) return false;
+    if (!consume(is, '>')) return false;
 
     return true;
 }
 
 bool compositions(std::istream& is) {
-    if(!composition(is)) return false;
-    if(lookahead(is) == '<') return compositions(is);
+    if (!composition(is)) return false;
+    if (lookahead(is) == '<') return compositions(is);
     return true;
 }
 
 bool cars(std::istream& is) {
-    if(lookahead(is) == '-') {
+    if (lookahead(is) == '-') {
         return consume(is, '-');
-    } else if(lookahead(is) == '(') {
+    } else if (lookahead(is) == '(') {
         return consume(is, '(')
             && cars(is) && consume(is,')');
     }
@@ -47,8 +47,8 @@ bool cars(std::istream& is) {
 }
 
 bool loco(std::istream& is) {
-    if(!consume(is, '*')) return false;
-    if(lookahead(is) == '*') return loco(is);
+    if (!consume(is, '*')) return false;
+    if (lookahead(is) == '*') return loco(is);
     return true;
 }
 
@@ -58,28 +58,30 @@ bool open(std::istream& is) {
 
 bool train(std::istream& is) {
     // exit with false if first char is not '['
-    if(!(consume(is, '['))) return false;
+    if (!consume(is, '[')) return false;
 
     // var represents the middle part of train
     bool var {false};
-    if(lookahead(is) == '*') {
+    if (lookahead(is) == '*') {
         var = open(is);
-    } else if(lookahead(is) == '<') {
+    } else if (lookahead(is) == '<') {
         var = compositions(is);
     } else {
         return false;
     }
     
     // exit with false if first char is not ']'
-    if(!(consume(is, ']'))) return false;
+    if (!consume(is, ']')) return false;
 
     return var;
 }
 
 int main() {
-    if(train(std::cin)) {
+    if (train(std::cin)) {
         std::cout << "valid!\n";
-    } else std::cout << "invalid!\n";
+    } else {
+        std::cout << "invalid!\n";
+    }
 
     return 0;
 }
